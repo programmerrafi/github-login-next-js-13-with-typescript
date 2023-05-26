56055db98957c5d68bf009edca573c0fad9e2213
programmerrafi <mdrafiprogrammer@gmail.com>
2023-05-26 19:11:33 +0600

    next-13 learning completed

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+
+export default async function Home() {
+  const session = await getServerSession();
+  console.log('session: ', session);
+
+  // if (!session) {
+  //   redirect('/api/auth/signin');
+  // }
+
+  return (
+    <div>
+      <h1>Welcome to NextSpace!</h1>
+      <p>
+        A next-gen social media app to connect with frens inspired by MySpace
+      </p>
+      <p>To get started, sign up for an account</p>
+    </div>
+  );
+}
